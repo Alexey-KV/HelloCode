158b805497fd1ceb884515d6fecef463792595fd
Alexey Kuznetchov <Kuznetchovav@gmail.com>
2023-02-26 22:03:38 +0300

    addedCondition Masha IfElse

---
+++ Example005_ConditionIfElsed/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Please, enter User Name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha");
+{
+    Console.WriteLine ("Congratulatins, Masha");
+}
+else
+{
+    Console.Write("Hi, user ");
+    Console.WriteLine(username);
+}
